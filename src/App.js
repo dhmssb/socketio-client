43165b61d561cc20accd833d788edfd44e3c7f23
dhms <dhmssb@gmail.com>
2021-03-23 23:23:18 +0700

    start to deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { UserContext } from './UserContext';
+import Chat from './components/chat/Chat';
+import Home from './components/home/Home';
+import Navbar from './components/layout/Navbar';
+import Login from './components/auth/Login';
+import Signup from './components/auth/Signup';
 function App() {
+  const [user, setUser] = useState(null)
+  useEffect(() => {
+    const verifyUser = async () => {
+      try {
+        const res = await fetch('http://localhost:5000/verifyuser', {
+          credentials: 'include',
+          headers: { 'Content-Type': 'application/json' }
+        });
+        const data = await res.json();
+        setUser(data);
+      } catch (error) {
+        console.log(error)
+      }
+
+
+    }
+    verifyUser()
+
+
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <UserContext.Provider value={{ user, setUser }}>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/chat/:room_id/:room_name" component={Chat} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+
+          </Switch>
+        </UserContext.Provider>
+      </div>
+    </Router>
   );
 }
 
